547b29c24df9dd47786e6f1dadb53e4cf0ec95e9
André Twupack <atwupack@mailbox.org>
2024-12-29 10:48:19 +0100

    upgrade to Bevy 0.15

--- bevy_shooter/src/main.rs
+++ bevy_shooter/src/main.rs
@@ -1,6 +1,4 @@
 use bevy::prelude::*;
-use bevy::render::view::VisibilitySystems;
-use bevy::sprite::Anchor;
 use crate::defs::{PLAY_RELOAD, PLAYER_BULLET_SPEED, PLAYER_SPEED, SCREEN_HEIGHT, SCREEN_WIDTH};
 
 mod defs;
@@ -19,11 +17,11 @@ fn main() {
             })
             .set(ImagePlugin::default_linear())
         )
-        .add_startup_system(setup)
-        .insert_resource(ClearColor(Color::rgb_u8(32, 32, 32)))
-        .add_system(move_player.before(apply_velocity))
-        .add_system(shoot_player_bullet.before(apply_velocity))
-        .add_system(apply_velocity)
+        .add_systems(Startup, setup)
+        .insert_resource(ClearColor(Color::srgb_u8(32, 32, 32)))
+        .add_systems(Update, move_player.before(apply_velocity))
+        .add_systems(Update, shoot_player_bullet.before(apply_velocity))
+        .add_systems(Update, apply_velocity)
         .run();
 }
 
@@ -40,22 +38,16 @@ struct Velocity(Vec2);
 struct PlayerShootTimer(Timer);
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2d::default());
     let mut timer = Timer::from_seconds(PLAY_RELOAD, TimerMode::Once);
     timer.pause();
     commands.spawn((
         Player,
         PlayerShootTimer(timer),
         Velocity(Vec2::default()),
-        SpriteBundle {
-            texture: asset_server.load("gfx/player.png"),
-            transform: Transform::from_xyz(-540., 260., 1.),
-            sprite: Sprite {
-                anchor: Anchor::CenterLeft,
-                ..default()
-            },
-            ..default()
-        })
+        Sprite::from_image(asset_server.load("gfx/player.png")),
+
+        )
     );
 }
 
@@ -66,51 +58,37 @@ fn is_entity_visible(transform: &Transform, image: &Image) -> bool {
     true
 }
 
-fn despawn_invisible_player_bullets(mut commands: Commands, query: Query<Entity, With<PlayerBullet>>) {
-    for entity in query.iter() {
-        commands.entity(entity).despawn();
-    }
-}
-
-fn move_player(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Velocity, With<Player>>) {
+fn move_player(keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<&mut Velocity, With<Player>>) {
     let mut player_velocity = query.single_mut();
 
     player_velocity.0.x = 0.0;
     player_velocity.0.y = 0.0;
-    if keyboard_input.pressed(KeyCode::Up) {
+    if keyboard_input.pressed(KeyCode::ArrowUp) {
         player_velocity.0.y = PLAYER_SPEED;
     }
-    if keyboard_input.pressed(KeyCode::Down) {
+    if keyboard_input.pressed(KeyCode::ArrowDown) {
         player_velocity.0.y = -PLAYER_SPEED;
     }
-    if keyboard_input.pressed(KeyCode::Left) {
+    if keyboard_input.pressed(KeyCode::ArrowLeft) {
         player_velocity.0.x = -PLAYER_SPEED;
     }
-    if keyboard_input.pressed(KeyCode::Right) {
+    if keyboard_input.pressed(KeyCode::ArrowRight) {
         player_velocity.0.x = PLAYER_SPEED;
     }
 }
 
-fn shoot_player_bullet(time: Res<Time>, mut commands: Commands, asset_server: Res<AssetServer>, keyboard_input: Res<Input<KeyCode>>, mut query: Query<(&Transform, &mut PlayerShootTimer), With<Player>>) {
+fn shoot_player_bullet(time: Res<Time>, mut commands: Commands, asset_server: Res<AssetServer>, keyboard_input: Res<ButtonInput<KeyCode>>, mut query: Query<(&Transform, &mut PlayerShootTimer), With<Player>>) {
     let (player_transform, mut shoot_timer) = query.single_mut();
     shoot_timer.0.tick(time.delta());
 
-    if keyboard_input.pressed(KeyCode::LShift) && (shoot_timer.0.paused() || shoot_timer.0.finished()) {
+    if keyboard_input.pressed(KeyCode::ShiftLeft) && (shoot_timer.0.paused() || shoot_timer.0.finished()) {
         shoot_timer.0.reset();
         shoot_timer.0.unpause();
         commands.spawn((
             PlayerBullet,
             Velocity(Vec2::new(PLAYER_BULLET_SPEED, 0.)),
-            SpriteBundle {
-                texture: asset_server.load("gfx/playerBullet.png"),
-                transform: player_transform.clone(),
-                sprite: Sprite {
-                    anchor: Anchor::CenterLeft,
-                    ..default()
-                },
-                ..default()
-            })
-        );
+            Sprite::from_image(asset_server.load("gfx/playerBullet.png"))
+        ));
     };
 }
 
@@ -120,8 +98,8 @@ fn apply_velocity(time: Res<Time>, mut query: Query<(&mut Transform, &Velocity)>
     let mut count = 0;
     for (mut transform, velocity) in &mut query {
         count += 1;
-        transform.translation.x += velocity.0.x * time.delta_seconds();
-        transform.translation.y += velocity.0.y * time.delta_seconds();
+        transform.translation.x += velocity.0.x * time.delta_secs();
+        transform.translation.y += velocity.0.y * time.delta_secs();
     }
     info!("Found {} entities.", count)
 }
